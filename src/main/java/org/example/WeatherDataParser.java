0397ec16656d21a7da45cfe2b77c3d1636d2684b
Davinci803 <130865965+Davinci803@users.noreply.github.com>
2023-08-08 16:18:46 +0300

    Update WeatherDataParser.java
    Добавил максимальную температуру

--- src/main/java/org/example/WeatherDataParser.java
+++ src/main/java/org/example/WeatherDataParser.java
@@ -2,8 +2,8 @@ package org.example;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
+
+import java.time.*;
 import java.time.format.DateTimeFormatter;
 public class WeatherDataParser {
     public static void parseWeatherData(String city, String jsonData) {
@@ -11,7 +11,7 @@ public class WeatherDataParser {
             ObjectMapper objectMapper = new ObjectMapper();
             JsonNode rootNode = objectMapper.readTree(jsonData);
             JsonNode weatherList = rootNode.get("list");
-
+            System.out.println(weatherList);
             System.out.println("Погода для города " + city + ":");
 
             DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
@@ -23,18 +23,23 @@ public class WeatherDataParser {
 
                 if (dateTime.toLocalDate().isEqual(today)) {
                     JsonNode mainData = weatherItem.get("main");
+                    JsonNode sunsetData = weatherItem.get("sys");
                     JsonNode weatherData = weatherItem.get("weather");
                     JsonNode weather = weatherData.get(0);
+                    //System.out.println(mainData.toPrettyString());
 
                     double temperatureKelvin = mainData.get("temp").asDouble();
                     double temperatureCelsius = temperatureKelvin - 273.15;
                     String weatherDescription = weather.get("description").asText();
-                    int pressure = mainData.get("pressure").asInt();
+                    int MaxtemperatureKelvin = mainData.get("temp_max").asInt();
+                    double MaxTemperatureCelsius = MaxtemperatureKelvin - 273.15;
+
 
                     System.out.println("Дата и время: " + dateTimeStr);
                     System.out.println("Температура: " + (int)temperatureCelsius + "°C");
+                    System.out.println("Максимальная температура: " + (int)MaxTemperatureCelsius + "°C");
                     System.out.println("Погода: " + weatherDescription);
-                    // System.out.println("Давление: " + pressure + " гПа");
+                    System.out.println("Закат: ");
                     System.out.println("---------------------------");
                 }
             }
